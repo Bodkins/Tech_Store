bcde353f0beb87f90b2b7c2692904b7d913a219f
Bodkins <42620119+Bodkins@users.noreply.github.com>
2022-08-19 23:02:12 -0600

    Index

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const menuMobileIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCartIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
@@ -42,3 +44,61 @@ function toggleCartAside(){
     aside.classList.toggle('inactive');
     
 }
+
+const productList= [];
+
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: 'Celphone',
+    price: 520,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: 'laptop',
+    price: 1220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+function renderProducts(products){
+    for (product of products){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+    
+        const ProductImg= document.createElement('img');
+        ProductImg.setAttribute('src', product.image)
+    
+        
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+        const productPrice = document.createElement('p');
+        productPrice.innerText= '$'+ product.price;
+        const productName = document.createElement('p');
+        productName.innerText= product.name;
+    
+        productInfoDiv.append(productPrice,productName)
+    
+        const productInfoFigure = document.createElement('figure');
+    
+        const productImgCart= document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg')
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.append(productInfoDiv, productInfoFigure);
+    
+        productCard.append(ProductImg,productInfo);
+    
+        cardsContainer.append(productCard);
+    }396
+}
+
+renderProducts(productList);
